bb7297fc6ff3cd94e7c08ad30474a2ef67579683
Pasquale Ambrosini <pasquale.ambrosini@gmail.com>
2017-09-30 14:54:41 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	var redirect = getUrlParameter("website");
+	if (!redirect) { return; }
+	var url = window.location.href;
+	redirect += "?"+url.substr(url.indexOf('&')+1);
+	window.location.replace(redirect);
+});
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
